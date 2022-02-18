fd0216ca620faf5e263c2cfcf51b669067a7ec74
DmitrySokoltsov <86969547+dmitrysokoltsov@users.noreply.github.com>
2022-02-18 14:34:06 +0200

    Initial Commit

---
+++ Item Sapce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Item Sapce
+//
+//  Created by Dmitry Sokoltsov on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
