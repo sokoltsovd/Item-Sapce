4102c28e27223f4d388d087e20d356a4c108664e
DmitrySokoltsov <86969547+dmitrysokoltsov@users.noreply.github.com>
2022-02-18 16:27:52 +0200

    Version 1

--- Item Sapce/ViewController.swift
+++ Item Sapce/ViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let dotaButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("DOTA 2", for: .normal)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.layer.cornerRadius = 30
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    let csgoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("CS:GO", for: .normal)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        button.layer.cornerRadius = 30
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    let selectItemImageView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .black
+        image.image = UIImage(named: "chest")
+        image.contentMode = .scaleAspectFit
+        image.layer.borderWidth = 5
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return image
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        SetConstraints()
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        selectItemImageView.layer.cornerRadius = selectItemImageView.frame.width / 2
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .black
+        view.addSubview(selectItemImageView)
+    }
+    
+    
 }
 
+//MARK: - SetConstraints
+
+extension ViewController {
+    
+    private func SetConstraints() {
+        
+        NSLayoutConstraint.activate([
+            selectItemImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            selectItemImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            selectItemImageView.heightAnchor.constraint(equalToConstant: 300),
+            selectItemImageView.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
